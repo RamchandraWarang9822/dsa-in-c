fbdc819ffeede2bc2c506a5fd8be486c548bd36e
Ramchandra Warang <92023869+RamchandraWarang9822@users.noreply.github.com>
2024-01-09 12:19:27 +0530

    Create BinarySearchTree.C

---
+++ BinarySearchTree.C
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int val;
+    struct node *left;
+    struct node *right;
+};
+
+struct node* newNode(int val)
+{
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->val = val;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+struct node* insert(struct node* node, int val){
+    if(node == NULL){
+        return newNode(val);
+    }
+    
+    if(node->val < val){
+        node->right = insert(node->right, val);
+    } else if(node->val > val){
+        node->left = insert(node->left, val);
+    }
+    
+    return node;
+}
+
+void inorder(struct node* root){
+    if(root != NULL){
+        inorder(root->left);
+        printf("%d ", root->val);
+        inorder(root->right);
+    }
+}
+
+void bfs(struct node* root){
+    if(root == NULL){
+        return;
+    }
+   
+    struct node** queue = (struct node**)malloc(sizeof(struct node*) * 1000);
+    int front  = -1, rear = -1;
+    
+    queue[++rear] = root;
+    
+    while(front != rear){
+        struct node* current = queue[++front];
+        printf("%d ", current->val);
+        
+        if(current->left != NULL){
+            queue[++rear] = current->left;
+        }
+        
+        if(current->right != NULL){
+            queue[++rear] = current->right;
+        }
+    }
+    
+    // free(queue);
+}
+
+int main(void) {
+    
+    struct node* root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+ 
+    // Print inorder traversal of the BST
+    bfs(root);
+    
+    return 0;
+}
